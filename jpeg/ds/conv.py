bb4a6c571217c6573bf0f83300162fb2ad910cf2
ahmadsalimi <ahsa9978@gmail.com>
2023-11-24 01:24:44 -0500

    Cleanup the code and add final conv

--- jpeg/ds/conv.py
+++ jpeg/ds/conv.py
@@ -1,19 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
 import torch
 from torch import nn
-import torch.nn.functional as F
 
-
-class Clamp(nn.Module):
-
-    def __init__(self, min_: float = 0.0, max_: float = 1.0):
-        super().__init__()
-        self.min = min_
-        self.max = max_
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x.clamp(self.min, self.max)
+from jpeg.utils import Clamp, pad_or_crop
 
 
 class ConvDownsample(nn.Module):
@@ -64,15 +54,26 @@ class ConvUpsample(nn.Module):
         factor (Tuple[int, int]): The upsampling factor along the height and width dimension.
             For instance, for 4:2:0 upsampling, use ``factor=(2, 2)``, for 4:2:2 upsampling,
             use ``factor=(1, 2)``. Defaults to (2, 2).
+        final_kernel_size (Optional[int]): The size of the final convolution kernel. If ``None``,
+            no final convolution is applied. Defaults to 3.
     """
 
-    def __init__(self, channels: int, kernel_size: int = 2, factor: Tuple[int, int] = (2, 2)):
+    def __init__(self, channels: int, kernel_size: int = 2, factor: Tuple[int, int] = (2, 2),
+                 final_kernel_size: Optional[int] = 3):
         super().__init__()
         self.conv = nn.Sequential(                                                  # B x 2 x H/f x W/f
             nn.Conv2d(2, channels, 1),                         # B x C x H/f x W/f
             nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(channels, 2, kernel_size,                # B x 2 x H x W
-                               stride=factor, padding=(kernel_size - factor[0]) // 2),
+            *([
+                  nn.ConvTranspose2d(channels, 2, kernel_size,          # B x 2 x H x W
+                                     stride=factor, padding=(kernel_size - factor[0]) // 2),
+              ] if final_kernel_size is None else [
+                nn.ConvTranspose2d(channels, channels, kernel_size,                 # B x C x H x W
+                                   stride=factor, padding=(kernel_size - factor[0]) // 2),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(channels, 2, final_kernel_size,               # B x 2 x H x W
+                          padding=(final_kernel_size - 1) // 2),
+            ]),
             Clamp(0, 1),
         )
 
@@ -88,13 +89,5 @@ class ConvUpsample(nn.Module):
             torch.Tensor: The upsampled images with shape :math:`(B, 3, H, W)`.
         """
         cbcr = self.conv(cbcr)                                  # B x 2 x H x W
-        if cbcr.shape[-2] > y.shape[-2]:
-            cbcr = cbcr[..., :y.shape[-2]-cbcr.shape[-2], :]
-        elif cbcr.shape[-2:] < y.shape[-2:]:
-            cbcr = F.pad(cbcr, (0, 0, 0, y.shape[-2] - cbcr.shape[-2]))
-        if cbcr.shape[-1] > y.shape[-1]:
-            cbcr = cbcr[..., :y.shape[-1]-cbcr.shape[-1]]
-        elif cbcr.shape[-1] < y.shape[-1]:
-            cbcr = F.pad(cbcr, (0, y.shape[-1] - cbcr.shape[-1]))
-        assert cbcr.shape[-2:] == y.shape[-2:], f"{cbcr.shape[-2:]} != {y.shape[-2:]}"
+        cbcr = pad_or_crop(cbcr, y.shape[-2:])                  # B x 2 x H x W
         return torch.cat([y, cbcr], dim=1)
---
+++ jpeg/utils.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class Clamp(nn.Module):
+
+    def __init__(self, min_: float = 0.0, max_: float = 1.0):
+        super().__init__()
+        self.min = min_
+        self.max = max_
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.clamp(self.min, self.max)
+
+
+def pad_or_crop(image: torch.Tensor, shape: torch.Size) -> torch.Tensor:
+    """Pad or crop an image to match the size of a base image.
+
+    Args:
+        image (torch.Tensor): The image to pad or crop with shape :math:`(B, C, H, W)`.
+        shape (torch.Size): The shape of the base image.
+
+    Returns:
+        torch.Tensor: The padded or cropped image with shape :math:`(B, C, H, W)`.
+    """
+    if image.shape[-2] > shape[-2]:
+        image = image[..., :shape[-2] - image.shape[-2], :]
+    elif image.shape[-2:] < shape[-2:]:
+        image = F.pad(image, (0, 0, 0, shape[-2] - image.shape[-2]))
+    if image.shape[-1] > shape[-1]:
+        image = image[..., :shape[-1] - image.shape[-1]]
+    elif image.shape[-1] < shape[-1]:
+        image = F.pad(image, (0, shape[-1] - image.shape[-1]))
+    assert image.shape[-2:] == shape[-2:], f"{image.shape[-2:]} != {shape[-2:]}"
+    return image
