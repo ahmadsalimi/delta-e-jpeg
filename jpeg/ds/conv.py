19c39bc46eef7f95aec86acdde1984ff0888a05c
ahmadsalimi <ahsa9978@gmail.com>
2023-11-26 00:56:53 -0500

    Add OtherUpsample

--- jpeg/ds/conv.py
+++ jpeg/ds/conv.py
@@ -2,6 +2,7 @@ from typing import Tuple, Optional
 
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 from jpeg.utils import Clamp, pad_or_crop, check_nan, NanChecker
 
@@ -85,6 +86,50 @@ class ConvDownsample(nn.Module):
         return y, cbcr
 
 
+class OtherUpsample(nn.Module):
+    """Upsample a batch of images using convolution.
+
+    Args:
+        channels (int): The number of intermediate channels.
+        kernel_size (int): The size of the convolution kernel. Defaults to 2.
+        factor (Tuple[int, int]): The upsampling factor along the height and width dimension.
+            For instance, for 4:2:0 upsampling, use ``factor=(2, 2)``, for 4:2:2 upsampling,
+            use ``factor=(1, 2)``. Defaults to (2, 2).
+        final_kernel_size (Optional[int]): The size of the final convolution kernel. If ``None``,
+            no final convolution is applied. Defaults to 3.
+    """
+
+    def __init__(self, channels: int, final_kernel_size: int = 3, clamp: bool = True):
+        super().__init__()
+        self.channels = channels
+        self.final_kernel_size_ = final_kernel_size
+        self.conv = nn.Sequential(                                                  # B x 2 x H x W
+            nn.Conv2d(2, channels, final_kernel_size, padding=(final_kernel_size - 1) // 2),  # B x C x H x W
+            nn.Dropout2d(0.5),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels, 2, 1),                            # B x 2 x H x W
+            *([] if not clamp else [
+                Clamp(-0.5, 0.5),
+                NanChecker('upsample->conv->clamp'),
+            ]),
+        )
+
+    def forward(self, y: torch.Tensor, cbcr: torch.Tensor) -> torch.Tensor:
+        """Upsample a batch of images.
+
+        Args:
+            y (torch.Tensor): A batch of luminance channels to upsample with shape :math:`(B, 1, H, W)`.
+            cbcr (torch.Tensor): A batch of chrominance channels to upsample with shape
+                :math:`(B, 2, H / factor[0], W / factor[1])`.
+
+        Returns:
+            torch.Tensor: The upsampled images with shape :math:`(B, 3, H, W)`.
+        """
+        cbcr = F.interpolate(cbcr, size=y.shape[-2:], mode='nearest')
+        cbcr = self.conv(cbcr)                                  # B x 2 x H x W
+        return torch.cat([y, cbcr], dim=1)
+
+
 class ConvUpsample(nn.Module):
     """Upsample a batch of images using convolution.
 
@@ -107,7 +152,6 @@ class ConvUpsample(nn.Module):
         self.conv = nn.Sequential(                                                  # B x 2 x H/f x W/f
             nn.Conv2d(2, channels, 1),                         # B x C x H/f x W/f
             nn.Dropout2d(0.5),
-            NanChecker('upsample->conv->1'),
             nn.ReLU(inplace=True),
             NanChecker('upsample->conv->relu'),
             *([
