2a587ea46714039dfb66ec36c65619f97fe26007
ahmadsalimi <ahsa9978@gmail.com>
2023-11-25 21:48:13 -0500

    Add nan checks

--- metric/delta_e.py
+++ metric/delta_e.py
@@ -87,33 +87,62 @@ class DeltaE2000(nn.Module):
         Returns:
             torch.Tensor: The delta E between the two batches of images with shape :math:`(B)`.
         """
+        def check_nan(val: torch.Tensor, name: str) -> None:
+            if torch.isnan(val).any():
+                raise ValueError(f"NaN detected in {name}.")
         l_x, a_x, b_x = self.__lab(x)                       # B x H x W
+        check_nan(l_x, 'l_x')
+        check_nan(a_x, 'a_x')
+        check_nan(b_x, 'b_x')
         l_y, a_y, b_y = self.__lab(x_hat)                   # B x H x W
+        check_nan(l_y, 'l_y')
+        check_nan(a_y, 'a_y')
+        check_nan(b_y, 'b_y')
         delta_l = l_y - l_x                                 # B x H x W
+        check_nan(delta_l, 'delta_l')
         l_bar = self.__bar(l_x, l_y)                    # B x H x W
+        check_nan(l_bar, 'l_bar')
         c_x = self.__c(x)                                   # B x H x W
+        check_nan(c_x, 'c_x')
         c_y = self.__c(x_hat)                               # B x H x W
+        check_nan(c_y, 'c_y')
         c_bar = self.__bar(c_x, c_y)                    # B x H x W
+        check_nan(c_bar, 'c_bar')
         a_x_prime = self.__a_prime(a_x, c_bar)              # B x H x W
+        check_nan(a_x_prime, 'a_x_prime')
         a_y_prime = self.__a_prime(a_y, c_bar)              # B x H x W
+        check_nan(a_y_prime, 'a_y_prime')
         c_x_prime = self.__c_prime(a_x_prime, b_x)          # B x H x W
+        check_nan(c_x_prime, 'c_x_prime')
         c_y_prime = self.__c_prime(a_y_prime, b_y)          # B x H x W
+        check_nan(c_y_prime, 'c_y_prime')
         delta_c_prime = c_y_prime - c_x_prime               # B x H x W
+        check_nan(delta_c_prime, 'delta_c_prime')
         c_bar_prime = self.__bar(c_x_prime, c_y_prime)  # B x H x W
+        check_nan(c_bar_prime, 'c_bar_prime')
         h_x = self.__h(a_x_prime, b_x)                      # B x H x W
+        check_nan(h_x, 'h_x')
         h_y = self.__h(a_y_prime, b_y)                      # B x H x W
+        check_nan(h_y, 'h_y')
         delta_h = self.__delta_h(h_x, h_y)                  # B x H x W
+        check_nan(delta_h, 'delta_h')
         delta_H = 2 * (c_x_prime * c_y_prime).sqrt() * \
             torch.sin(torch.deg2rad(delta_h) / 2)           # B x H x W
+        check_nan(delta_H, 'delta_H')
         h_bar = self.__h_bar(h_x, h_y)                      # B x H x W
+        check_nan(h_bar, 'h_bar')
         t = 1 - 0.17 * torch.cos(torch.deg2rad(h_bar - 30)) + \
             0.24 * torch.cos(torch.deg2rad(2 * h_bar)) + \
             0.32 * torch.cos(torch.deg2rad(3 * h_bar + 6)) - \
             0.2 * torch.cos(torch.deg2rad(4 * h_bar - 63))  # B x H x W
+        check_nan(t, 't')
         s_l = 1 + 0.015 * (l_bar - 50) ** 2 / \
             (20 + (l_bar - 50) ** 2).sqrt()                 # B x H x W
+        check_nan(s_l, 's_l')
         s_c = 1 + 0.045 * c_bar_prime                       # B x H x W
+        check_nan(s_c, 's_c')
         s_h = 1 + 0.015 * c_bar_prime * t                   # B x H x W
+        check_nan(s_h, 's_h')
         r_t = -2 * (c_bar_prime ** 7 / (c_bar_prime ** 7 + 25 ** 7)).sqrt() * \
             torch.sin(
                 torch.deg2rad(
@@ -121,9 +150,11 @@ class DeltaE2000(nn.Module):
                         -((h_bar - 275) / 25) ** 2)
                 )
             )                                               # B x H x W
+        check_nan(r_t, 'r_t')
         delta_e = (delta_l / (self.k_l * s_l)) ** 2 + \
             (delta_c_prime / (self.k_c * s_c)) ** 2 + \
             (delta_H / (self.k_h * s_h)) ** 2 + \
             r_t * (delta_c_prime / (self.k_c * s_c)) * \
             (delta_H / (self.k_h * s_h))                    # B x H x W
+        check_nan(delta_e, 'delta_e')
         return delta_e.mean()
